c3bcd0fa8b8b67cee645898c827afddb93cae6a2
kytoaq <kytoaq@gmail.com>
2020-07-29 02:56:50 +0000

    [File]: Add license and separator
    Add License APACHE-2.0
    
    - Include filter: Doha
    - Include # Separator

--- Android.mk
+++ Android.mk
@@ -1,6 +1,35 @@
-LOCAL_PATH := $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#
+# Copyright 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter doha, $(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
+# Camera IMS
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 IMS_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -11,6 +40,7 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+# NXP Libs
 NXP_LIB := libstnfc_nci_jni.so libnfc_st_dta_jni.so
 NXP_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT_APPS)/Nfc_st/lib/arm64/,$(notdir $(NXP_LIB)))
 $(NXP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
